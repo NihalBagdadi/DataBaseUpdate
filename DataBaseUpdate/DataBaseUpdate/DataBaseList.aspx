2f000505da24e78d327cf6864acd0a0e52e95ce9
Nihal Bagdadi <nihalbagdadi1995@gmail.com>
2021-10-02 14:56:24 +0530

    Make a list of databases

---
+++ DataBaseUpdate/DataBaseUpdate/DataBaseList.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataBaseList.aspx.cs" Inherits="DataBaseUpdate.DataBaseList" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:DropDownList ID="drpDataBaseList1" runat="server"></asp:DropDownList>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataBaseUpdate/DataBaseUpdate/DataBaseList.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace DataBaseUpdate
+{
+    public partial class DataBaseList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string serverName = @"(LocalDB)\MSSQLLocalDB";
+            string userid = "";
+            string password = "";
+            string database = "master";
+            SqlConnection con; 
+            using (con = new SqlConnection("server=" + serverName + ";uid=" + userid + ";pwd=" + password + ";database=" + database + ""))
+            {
+                SqlCommand cmd = new SqlCommand("select name from sys.database",con);
+                con.Open();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        drpDataBaseList1.Items.Add(dr[0].ToString());
+                    }
+                }
+               
+            }
+
+        }
+    }
+}
